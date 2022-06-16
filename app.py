25d3e21fabb7c2a54854fd1f7e6c693fdd56269f
tassoman <tassoman@gmail.com>
2022-06-16 22:47:03 +0200

    pytz supported

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, make_response
 from markupsafe import escape
 from ics import Calendar, Event
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, time
+import pytz
+
+tz_ita = pytz.timezone('Europe/Rome')
 
 app = Flask(__name__)
 
@@ -14,15 +17,35 @@ def hello(name):
     return f"Hello, {escape(name)}!"
 
 @app.route('/domani')
-def domani():
-    domani = datetime.now() + timedelta(days=1)
+@app.route('/domani/')
+@app.route('/domani/<path:quando>')
+def domaniQuando(quando=None):
+    if quando == 'mattina':
+        hours = timedelta(hours=10)
+    elif quando == 'pomeriggio':
+        hours = timedelta(hours=14)
+    else:
+        hours = timedelta(hours=9)
+
+    if escape(quando) != 'None':
+        inizio = datetime.combine(
+            datetime.now(tz=tz_ita) + timedelta(days=1), 
+            time(0,0)
+        ) + hours
+    else:
+        inizio = datetime.now(tz=tz_ita) + timedelta(days=1)
+
     c = Calendar()
     e = Event()
     e.name = "Ci aggiorniamo Domani"
-    e.begin = domani
-    e.end = domani + timedelta(hours=1)
+
+    if escape(quando) != 'None':
+        e.name = e.name + ', ' + escape(quando)
+
+    e.begin = inizio
+    e.end = inizio + timedelta(hours=1)
     c.events.add(e)
     c.events
     r = make_response(str(c))
-    r.headers["Content-Disposition"] = "attachment; filename=domani.ics"
+    r.headers["Content-Disposition"] = "attachment; filename=ci-aggiorniamo.ics"
     return r
